4910f1fa4bbc25f36c2beaa7bf77f98f3065c1cc
mihail <mihail.workbuz@gmail.com>
2018-09-03 00:54:04 +0300

    add default_scope to answer and comment

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ApplicationRecord
   belongs_to :med_case
   has_many :comments, as: :commentable
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  default_scope { order(created_at: :desc) }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  default_scope { order(created_at: :desc) }
 end
