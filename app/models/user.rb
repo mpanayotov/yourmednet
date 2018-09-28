cfbe4bcb65e130d29cb7c42bcc40f9c84887b7fa
mihail <mihail.workbuz@gmail.com>
2018-09-28 18:01:15 +0300

    minor improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
-
   devise :database_authenticatable
-
   belongs_to :profile, polymorphic: true
-
   has_many :med_cases
-
   has_many :answers
 
   def display_name
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,31 +9,3 @@ DoctorProfile.create(specialty: 'Gastroenterologist', user: user2, picture: pict
 user3 = User.new(email: 'admin3@gmail.com', name: 'Valentin Petrov', password: 'admin3')
 picture3 = File.new('public/doctor3.png')
 DoctorProfile.create(specialty: 'Orthopaedic', user: user3, picture: picture3, workplace: 'Medical Centre Saint Sofia')
-
-# "Born and raised in Stara Zagora, Bulgaria.
-
-# Graduated from Romaind Rolland Foreign Language School in 2001.
-
-# High School Profile: English with German.
-
-# Graduated from Sofia University and acquired my medical degree in 2007.
-
-# Started specialization in Gastroenterology in autumn 2008.
-
-# Started working at SantraMed Hospital in 2009.
-
-
-# Attended European Gastroenterology conference in Berlin, Germany in 2010.
-
-# Attended two Gastroenterology seminars in Athens, Greece in 2011, 2012.
-
-# Member of Bulgarian Gastroenterology association since 2012.
-
-# Member of Bulgarian Healthcare association since 2013.
-
-# Co-author of several scientific pieces of work in the field of Gastroenterology.
-
-# Assistant teacher at Faculty of Medicine, Sofia University since 2015.
-
-# Languages: English, German, Russian.
-# "
